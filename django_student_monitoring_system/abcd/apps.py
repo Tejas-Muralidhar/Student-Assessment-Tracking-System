99fe99b3006d91661b1abab982070a310f4434bf
Tejas <1rn21cs171.Tejas@gmail.com>
2024-02-11 13:35:35 +0530

    .

--- django_student_monitoring_system/abcd/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbcdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abcd'
--- django_student_monitoring_system/abcd/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import say_goodbye
-
-urlpatterns =[
-    path('abcd/',say_goodbye)
-]
